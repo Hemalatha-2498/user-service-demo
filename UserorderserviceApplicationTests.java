029bebb82e226a827353ed64ff2495dc0b01f567
Hemalatha-2498 <hemalathak9566@gmail.com>
2026-01-22 13:50:45 +0530

    Add files via upload
    Added src folder with application code

---
+++ UserorderserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.userorderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserorderserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
